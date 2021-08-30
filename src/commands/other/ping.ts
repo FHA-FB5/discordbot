ea07f486faeddb40e588c90dcb73bfc39815ccb4
Titus <tituskirch1999@gmail.com>
2021-08-30 20:47:55 +0200

    use local files for commadn description

--- src/commands/other/ping.ts
+++ src/commands/other/ping.ts
@@ -1,11 +1,14 @@
 import { SlashCommandBuilder } from '@discordjs/builders';
 import { CommandInteraction } from 'discord.js';
+import { getMessage } from '@/utils';
 
 export default {
   name: 'ping',
+  cooldown: 10000,
+  defaultPermission: false,
   data: new SlashCommandBuilder()
     .setName('ping')
-    .setDescription('Replies with Pong!'),
+    .setDescription(getMessage('command.other.ping.description')),
   async execute(interaction: CommandInteraction) {
     await interaction.reply('Pong!');
   },
